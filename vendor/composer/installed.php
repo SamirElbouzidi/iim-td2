5787846d4c668dd819f3d547068f22ff1c5cd794
SamirElbouzidi <96955744+SamirElbouzidi@users.noreply.github.com>
2023-07-03 11:35:29 +0200

    First Commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'selbouzidi/iim-td2',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'f80984e011530bf23a877793bf1ea9b72c6f2490',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'phpstan/phpstan' => array(
+            'pretty_version' => '1.10.22',
+            'version' => '1.10.22.0',
+            'reference' => '97d694dfd4ceb57bcce4e3b38548f13ea62e4287',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpstan/phpstan',
+            'aliases' => array(),
+            'dev_requirement' => true,
+        ),
+        'selbouzidi/iim-td2' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'f80984e011530bf23a877793bf1ea9b72c6f2490',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
